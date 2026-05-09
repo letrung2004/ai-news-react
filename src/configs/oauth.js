3d35939c470d7adb98e2cd02130a6c54f60af886
letrung2004 <lequoctrunggg@gmail.com>
2026-05-10 00:08:29 +0700

    update env production

--- src/configs/oauth.js
+++ src/configs/oauth.js
@@ -1,5 +1,5 @@
 export const oauth = {
   clientId: "1068236266740-bvorau9ai9jsfn2bi3vbii1hghtpuv39.apps.googleusercontent.com",
-  redirectUri: "http://localhost:5173/authenticate",
-  authUri: "https://accounts.google.com/o/oauth2/auth",
+  // redirectUri: "http://localhost:5173/authenticate",
+  redirectUri: import.meta.env.VITE_REDIRECT_URI || "http://localhost:5173/authenticate",
 };
